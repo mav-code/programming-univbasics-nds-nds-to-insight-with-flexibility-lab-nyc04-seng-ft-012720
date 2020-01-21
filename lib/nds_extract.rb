0de56bc653a4776ede3564e3ad3bd392d76a2d7f
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-21 06:44:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -100,7 +100,7 @@ def movies_with_directors_set(source)
           moviehash[:title] = source[index][:movies][innerindex]
           moviehash[:director_name] = director
           moviehash[:studio] = source[index][:movies][innerindex][:studio]
-          moviehash
+          moviehash{:worldwide_gross] = source[index][:movies][innerindex][:worldwide_gross]
           moviearray << moviehash
           output << moviearray
           innerindex += 1
